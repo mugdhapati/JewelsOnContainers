4607e86537c28ac9d27c5e5953bd8a257f1259e9
Mugdha Pati <mugdhapati@gmail.com>
2020-06-09 07:08:14 -0700

    Initial commit

---
+++ ProductCatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogAPI.Data
+{
+    public class CatalogContext : DbContext                 //my classs inherit from DBContext(Database Context used by EntityFrameworkCore)
+    {
+        public DbSet<CatalogType> CatalogTypes { get; set; }               //DbSet - is a db table //make a table with name CatalogTypes
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }              //make a table with name CatalogBrands
+        public DbSet<CatalogItem> CatalogItems { get; set; }                 //make a table with name CatalogItems
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)          //Officially as a child you are override'ng your parents on modalcreating method  i.e., DbContext
+        {                   
+                                                                                   //provide your instructions on how to build the tables & ModelBuilder is the one to do this
+        }
+    }
+}

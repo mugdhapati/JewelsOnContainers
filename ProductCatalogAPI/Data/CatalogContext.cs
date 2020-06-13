0c49e55afdf8972fe222853d65ff4396d45b7c23
Mugdha Pati <mugdhapati@gmail.com>
2020-06-13 08:44:30 -0700

    Creating Tables Commit

--- ProductCatalogAPI/Data/CatalogContext.cs
+++ ProductCatalogAPI/Data/CatalogContext.cs
@@ -14,8 +14,42 @@ namespace ProductCatalogAPI.Data
         public DbSet<CatalogItem> CatalogItems { get; set; }                 //make a table with name CatalogItems
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)          //Officially as a child you are override'ng your parents on modalcreating method  i.e., DbContext
-        {                   
-                                                                                   //provide your instructions on how to build the tables & ModelBuilder is the one to do this
+        {
+            modelBuilder.Entity<CatalogBrand>(e =>                                                                     //provide your instructions on how to build the tables    & ModelBuilder is the one to do this
+            {
+                e.ToTable("CatalogBrands");
+                e.Property(b => b.Id).IsRequired()
+                                     .UseHiLo("catalog_brand_hilo");                          //UseHiLo is used to provide a auto generated number
+                e.Property(b => b.Brand)
+                            .IsRequired()
+                            .HasMaxLength(100);
+
+            });
+            modelBuilder.Entity<CatalogType>(e =>
+            {
+                e.ToTable("CatalogTypes");
+                e.Property(t => t.Id).IsRequired()
+                                    .UseHiLo("catalog_type_id");
+                e.Property(t => t.Type).IsRequired()
+                                        .HasMaxLength(100);
+            });
+            modelBuilder.Entity<CatalogItem>(e =>
+            {
+                e.ToTable("Catalog");
+                e.Property(c => c.Id).IsRequired()
+                                     .UseHiLo("catalog_hilo");
+                e.Property(c => c.Name).IsRequired()
+                                        .HasMaxLength(200);
+                e.Property(c => c.Price).IsRequired();
+
+                e.HasOne(c => c.CatalogType)
+                    .WithMany()
+                        .HasForeignKey(c => c.CatalogTypeId);
+                e.HasOne(c => c.CatalogBrand)
+                    .WithMany()
+                        .HasForeignKey(c => c.CatalogBrandId);
+
+            });
         }
     }
 }
--- ProductCatalogAPI/Domain/CatalogItem.cs
+++ ProductCatalogAPI/Domain/CatalogItem.cs
@@ -11,10 +11,9 @@ namespace ProductCatalogAPI.Domain
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-
         public string PictureUrl { get; set; }
         public int CatalogTypeId { get; set; }
-        public int CatalogItemId { get; set; }
+        public int CatalogBrandId { get; set; }
         public CatalogType CatalogType { get; set; } 
         public CatalogBrand CatalogBrand { get; set; }
     }
